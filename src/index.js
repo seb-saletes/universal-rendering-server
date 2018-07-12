1a3ccf7a9a03b37387947c040155a59c490a8469
Seb Saletes <seb.saletes@gmail.com>
2018-07-13 01:31:23 +0800

    correct script entry point

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { ApolloServer } from 'apollo-server'
 import './mongoose/_init'
 import Schema from './graphql/schema'
 
-const server = new ApolloServer({schema: Schema})
+const server = new ApolloServer({ schema: Schema })
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
