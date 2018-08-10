8c9240ca04dd73776137aa0af3a83a5d377fd859
Seb Saletes <seb.saletes@gmail.com>
2018-08-10 22:51:56 +0200

    revert change on express

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import { ApolloServer } from 'apollo-server-express'
-import cors from 'cors'
-import express from 'express'
-import cookieParser from 'cookie-parser'
+import { ApolloServer } from 'apollo-server'
 import './mongoose/_init'
 import Schema from './graphql/schema'
 
-const app = express()
-
-// enable cors
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true, // <-- REQUIRED backend setting
-}
 const server = new ApolloServer({
   schema: Schema,
   context: ({ req }) => ({
@@ -19,20 +9,8 @@ const server = new ApolloServer({
   }),
 })
 
-app.use(cors())
-
-//
-app.use((req, res, next) => {
-  res.set({
-    'access-control-allow-origin': 'localhost:3000',
-    'Access-Control-Allow-Origin': 'localhost:3000',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTION',
-  })
-  next()
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
 })
-
-app.use(cookieParser())
-server.applyMiddleware({ app })
-
-app.listen({ port: 4000 }, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`))
